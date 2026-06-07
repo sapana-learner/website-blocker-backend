09a1004b3b8e59217f29c0f61c276901e9c1e13f
blueberry-unicorn <75934194+blueberry-unicorn@users.noreply.github.com>
2026-06-07 18:07:56 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-
 from database import Base
 
 
@@ -12,15 +11,8 @@ class User(Base):
     email = Column(String, unique=True)
     password = Column(String)
 
-    websites = relationship(
-        "BlockedWebsite",
-        back_populates="user"
-    )
-
-    focus_sessions = relationship(
-        "FocusSession",
-        back_populates="user"
-    )
+    websites = relationship("BlockedWebsite", back_populates="user")
+    focus_sessions = relationship("FocusSession", back_populates="user")
 
 
 class BlockedWebsite(Base):
@@ -28,31 +20,16 @@ class BlockedWebsite(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     website = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
 
-    user_id = Column(
-        Integer,
-        ForeignKey("users.id")
-    )
-
-    user = relationship(
-        "User",
-        back_populates="websites"
-    )
+    user = relationship("User", back_populates="websites")
 
 
 class FocusSession(Base):
     __tablename__ = "focus_sessions"
 
     id = Column(Integer, primary_key=True, index=True)
-
     status = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
 
-    user_id = Column(
-        Integer,
-        ForeignKey("users.id")
-    )
-
-    user = relationship(
-        "User",
-        back_populates="focus_sessions"
-    )
+    user = relationship("User", back_populates="focus_sessions")
